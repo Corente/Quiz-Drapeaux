2b4517d2172e3df18b3371b6126538633e0b48d4
Corente <corente94@hotmail.fr>
2021-10-24 22:36:39 +0200

    added update to leaderboard with same name

--- app/database.py
+++ app/database.py
@@ -21,15 +21,23 @@ def database_get_leaderboard():
     file.close()
     return data
 
+def check_if_name_exist(name, database, score):
+    for i in database:
+        if i["name"] == name and i["score"] < score:
+            i["score"] = score
+            return True
+    return False
+
 def database_post_leaderboard(name, score):
     if (name == ""):
         return
     file = open(leaderboard_path, 'r+')
     data = json.load(file)
-    entry = {}
-    entry["name"] = name
-    entry["score"] = score
-    data.append(entry)
+    if (check_if_name_exist(name, data, score) == False):
+        entry = {}
+        entry["name"] = name
+        entry["score"] = score
+        data.append(entry)
     file.close()
     file = open(leaderboard_path, "w")
     json.dump(data, file)
