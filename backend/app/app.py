14359cfc80990142d7b562267e4e98c200092a3e
Corente <corente94@hotmail.fr>
2022-12-25 17:16:09 +0100

    [MAIN] updated backend

---
+++ backend/app/app.py
@@ -0,0 +1,30 @@
+from flask import Flask, jsonify, request
+from app.database import *
+from flask_cors import CORS
+import ast
+
+app = Flask(__name__)
+cors = CORS(app, resources={r'/api/*' : {'origins' : 'https://quiz.ourvoy.fr'}})
+
+@app.route('/api/country/<int:id>', methods=['GET'])
+def get_country(id):
+    return jsonify(database_get_country(id))
+
+@app.route('/api/leaderboard/', methods=['GET'])
+def get_leaderboard():
+    return jsonify(database_get_leaderboard())
+
+@app.route('/api/leaderboard', methods=['POST'])
+def post_leaderboard():
+    """
+    if (request.json == None):
+        data = request.data
+        data = ast.literal_eval(data.decode("utf-8"))
+    else:
+        data = request.json
+    """
+    data = request.json
+    name = data["name"]
+    score = data["score"]
+    database_post_leaderboard(name, score)
+    return {'request': 'sucess'}
---
+++ backend/app/database.py
@@ -0,0 +1,73 @@
+import json
+import os
+import boto3
+countries_path = "./files/db.json"
+leaderboard_path = "./files/leaderboard.json"
+bucket_name = os.environ["AWS_BUCKET_NAME"]
+
+s3 = boto3.resource(
+    service_name = "s3",
+    region_name = os.environ["AWS_REGION"],
+    aws_access_key_id = os.environ["AWS_SECRET_KEY_ID"],
+    aws_secret_access_key = os.environ["AWS_SECRET_ACCESS_KEY"]
+)
+
+def upload_file(filename):
+    if filename == "db":
+        s3.Bucket(bucket_name).upload_file(Filename=countries_path, Key="db.json")
+    elif filename == "leaderboard":
+        s3.Bucket(bucket_name).upload_file(Filename=leaderboard_path, Key="leaderboard.json")
+    else:
+        print("the fuck you are doing here")
+
+def download_file(filename):
+    if filename == "db":
+        s3.Bucket(bucket_name).download_file(Filename=countries_path, Key="db.json")
+    elif filename == "leaderboard":
+        s3.Bucket(bucket_name).download_file(Filename=leaderboard_path, Key="leaderboard.json")
+    else:
+        print("the fuck you are doing here")
+
+
+def database_get_country(id):
+    download_file("db")
+    file = open(countries_path)
+    data = json.load(file)
+    ret = {}
+    for i in data:
+        if int(i["id"]) == id:
+            ret = i
+            break
+    file.close()
+    return ret
+    
+def database_get_leaderboard():
+    download_file("leaderboard")
+    file = open(leaderboard_path, 'r')
+    data = json.load(file)
+    data.sort(key=lambda k: k['score'], reverse=True)
+    file.close()
+    return data
+
+def check_if_name_exist(name, database, score):
+    for i in database:
+        if i["name"] == name and i["score"] < score:
+            i["score"] = score
+            return True
+    return False
+
+def database_post_leaderboard(name, score):
+    if (name == ""):
+        return
+    file = open(leaderboard_path, 'r+')
+    data = json.load(file)
+    if (check_if_name_exist(name, data, score) == False):
+        entry = {}
+        entry["name"] = name
+        entry["score"] = score
+        data.append(entry)
+    file.close()
+    file = open(leaderboard_path, "w")
+    json.dump(data, file)
+    file.close()
+    upload_file("leaderboard")
